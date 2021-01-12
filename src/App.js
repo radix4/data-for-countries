56ef31ae473653c63410f11678a8c82a8a7245a9
Thang Cao <shiyoline@gmail.com>
2021-01-12 12:51:10 -0800

    display all countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  return <div></div>
+  const [countries, setCountries] = useState([])
+  const [newSearch, setNewSearch] = useState('')
+
+  // TODO: Use useEffect hook to fetch data
+  // Restful PI: https://restcountries.eu/rest/v2/all
+  useEffect(() => {
+    console.log('effect')
+    axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+      console.log('Promise fulfilled!')
+      setCountries(response.data)
+    })
+  }, [])
+
+  const onChangeSearch = event => {
+    setNewSearch(event.target.value)
+    //console.log(event.target.value)
+  }
+
+  // toLowerCase() makes search query case insensitive
+  function filterByName(country) {
+    return country.name.toLowerCase().indexOf(newSearch) !== -1
+  }
+
+  return (
+    <div>
+      find countries
+      <input value={newSearch} onChange={onChangeSearch}></input>
+      <div>
+        {countries.map(country => (
+          <div key={country.name}>{country.name}</div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
