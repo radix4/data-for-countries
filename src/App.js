b0a4421a3275b0a2af70ca2189900d96eda4e796
Thang Cao <shiyoline@gmail.com>
2021-01-12 14:44:37 -0800

    able to query for a single country for details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,48 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const DisplayCountries = props => {
+  let countriesByName = props.countries.filter(props.filterByName)
+
+  if (countriesByName.length === 1) {
+    let country = countriesByName[0]
+    let flag = country.flag
+    console.log(flag)
+
+    return (
+      <div>
+        <h1>{country.name}</h1>
+        <p>capital: {country.capital}</p>
+        <p>population: {country.population}</p>
+
+        <h4>Languages</h4>
+        <ul>
+          {country.languages.map(language => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <img
+          style={{ height: '100px' }}
+          src={flag}
+          alt='This is the flag of the country.'></img>
+      </div>
+    )
+  }
+
+  if (countriesByName.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  } else {
+    console.log(countriesByName)
+    return (
+      <div>
+        {countriesByName.map(country => (
+          <div key={country.name}>{country.name}</div>
+        ))}
+      </div>
+    )
+  }
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [newSearch, setNewSearch] = useState('')
@@ -30,11 +72,7 @@ const App = () => {
     <div>
       find countries
       <input value={newSearch} onChange={onChangeSearch}></input>
-      <div>
-        {countries.map(country => (
-          <div key={country.name}>{country.name}</div>
-        ))}
-      </div>
+      <DisplayCountries countries={countries} filterByName={filterByName} />
     </div>
   )
 }
